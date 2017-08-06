e017a5d9d6d1324a2ea85af30c382ec4f79552b6
tijmenvanegmond <nemjit.12@gmail.com>
2017-08-06 15:26:53 +0200

    Reworked camera

--- Assets/Scripts/OrbitCamera.cs
+++ Assets/Scripts/OrbitCamera.cs
@@ -1,58 +1,82 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using System.Collections;
+using System.Linq;
 
 public class OrbitCamera : MonoBehaviour
 {
+    public GameObject Target;
+    public float MinDistance = 3f;
+    public float MaxDistance = 100f;
+    public float Distance = 10f;
+    public Quaternion DefaultRotation = Quaternion.Euler(new Vector3(45f, 0, 0));
+    private Quaternion _addedRotation = Quaternion.identity;
+    private Vector3 _previousMousePos = Vector3.zero;
+    private float _lastXdelta = 0f;
 
-    public GameObject target;
-    bool animate = true;
-    Vector3 previousMousePos = Vector3.zero;
-    float lastXdelta = 5f;
-
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        Vector3 mouseDelta = (Input.mousePosition - previousMousePos) * Time.deltaTime;
+        Vector3 mouseDelta = (Input.mousePosition - _previousMousePos) * Time.deltaTime;
         mouseDelta = Vector3.ClampMagnitude(mouseDelta, 5f);
-        previousMousePos = Input.mousePosition;
-
-        if (Input.GetMouseButtonDown(4))
-        {
-            if (transform.parent == null)
-            {
-                animate = false;
-                transform.SetParent(target.transform);
-            }
-            else
-            {
-                animate = true;
-                transform.SetParent(null);
-            }
-        }
-
-
+        _previousMousePos = Input.mousePosition;
+        
         if (Input.GetMouseButton(2))
         {
-
             if (Mathf.Abs(mouseDelta.x) > Mathf.Abs(mouseDelta.y))
             {
-                transform.RotateAround(target.transform.position, Vector3.up, mouseDelta.x * 200f * Time.deltaTime);
+                _addedRotation *= Quaternion.Euler(new Vector3(0,  mouseDelta.x * 200f * Time.deltaTime, 0));
 
             }
             else
             {
-                //transform.RotateAround(target.transform.position, Vector3.left, mouseDelta.y * 200f * Time.deltaTime);
-                if (animate) transform.Translate(Vector3.down * mouseDelta.y * Time.deltaTime * 20);
+                _addedRotation *= Quaternion.Euler(new Vector3( mouseDelta.y * -200f * Time.deltaTime ,0f,  0f));
             }
 
-            lastXdelta = mouseDelta.x * 50;
+            _lastXdelta = mouseDelta.x * 50;
         }
-        else
-        {
-            transform.RotateAround(target.transform.position, Vector3.up, lastXdelta * Time.deltaTime);
-        }
-        transform.Translate(Vector3.forward * Input.mouseScrollDelta.y * Time.deltaTime * 20);
-
-        if (animate) transform.LookAt(target.transform.position);
+        
+        Distance -= Input.mouseScrollDelta.y * Time.deltaTime * 50;
+        Distance = Mathf.Clamp(Distance, MinDistance, MaxDistance);
+        
+        PlaceCameraWithRay();
     }
+
+    void PlaceCameraWithRay()
+    {
+        Vector3 cameraDirection = -(Target.transform.rotation*_addedRotation*DefaultRotation*Vector3.forward);
+        Ray ray = new Ray(Target.transform.position+(cameraDirection * Distance),-cameraDirection);
+
+        try
+        {
+            ray = GetPoinOfVisibilty(ray);
+        }
+        catch
+        {
+            Debug.Log("can't find the ship");
+        }
+        
+        Debug.DrawRay(ray.origin,ray.direction);
+        
+        transform.position = ray.origin + ray.origin.normalized;
+        
+        transform.rotation = Target.transform.rotation*_addedRotation* DefaultRotation;
+    }
+
+    Ray GetPoinOfVisibilty(Ray ray)
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, Distance))
+        {
+            if (hit.rigidbody != null && hit.rigidbody.gameObject == Target)
+            {
+                return ray;
+            }
+            else
+            {
+                return GetPoinOfVisibilty(new Ray(hit.point, ray.direction));
+            }
+        }
+        else throw new Exception("ray no aimed at hip or ship does not match ");
+    }
+
 }
--- Assets/Scripts/SpaceShipController.cs
+++ Assets/Scripts/SpaceShipController.cs
@@ -6,37 +6,45 @@ public class SpaceShipController : MonoBehaviour
 
     public Vector3 MovementMultiplier = Vector3.one;
     public Vector3 RotationMultiplier = Vector3.one;
-    private Vector3 movementInputVector;
-    private Vector3 rotationInputVector;
+    private Vector3 _movementInputVector;
+    private Vector3 _rotationInputVector;
     private bool _isDampening = false;
     private Rigidbody _rBody;
+    private float _drag;
+    private float _angularDrag;
 
     void Start() // Use this for initialization
     {
         _rBody = GetComponent<Rigidbody>();
+        _drag = _rBody.drag;
+        _angularDrag = _rBody.angularDrag;
     }
 
     void Update() // Update is called once per frame
     {
-        movementInputVector = new Vector3(Input.GetAxis("ShipXAxis"), Input.GetAxis("ShipYAxis"), Input.GetAxis("ShipZAxis"));
-        rotationInputVector = new Vector3(Input.GetAxis("ShipPitch"), Input.GetAxis("ShipYaw"), Input.GetAxis("ShipRoll"));
+        _movementInputVector = new Vector3(Input.GetAxis("ShipXAxis"), Input.GetAxis("ShipYAxis"), Input.GetAxis("ShipZAxis"));
+        _rotationInputVector = new Vector3(Input.GetAxis("ShipPitch"), Input.GetAxis("ShipYaw"), Input.GetAxis("ShipRoll"));
 
         if (Input.GetKeyUp(KeyCode.X)) _isDampening = !_isDampening;
+        
+        if (_isDampening)
+        {
+            //_rBody.AddForce(Vector3.Scale(-_rBody.velocity.normalized, MovementMultiplier) * Time.fixedDeltaTime);
+            //_rBody.AddTorque(Vector3.Scale(-_rBody.angularVelocity.normalized, RotationMultiplier) * Time.fixedDeltaTime);
+            _rBody.angularDrag = 1;
+            _rBody.drag = 1;
+        }
+        else
+        {
+            _rBody.drag = _drag;
+            _rBody.angularDrag = _angularDrag;
+        }
     }
 
     void FixedUpdate()
     {
-        _rBody.AddRelativeForce(Vector3.Scale(movementInputVector, MovementMultiplier) * Time.fixedDeltaTime);
-        _rBody.AddRelativeTorque(Vector3.Scale(rotationInputVector, RotationMultiplier) * Time.fixedDeltaTime);
-
-        if (_isDampening)
-        {
-            _rBody.AddForce(Vector3.Scale(-_rBody.velocity.normalized, MovementMultiplier) * Time.fixedDeltaTime);
-            _rBody.AddTorque(Vector3.Scale(-_rBody.angularVelocity.normalized, RotationMultiplier) * Time.fixedDeltaTime);
-        }
-
-        //_rBody.drag = _isDampening ? 0.4f : 0;
-        //_rBody.angularDrag = _isDampening ? 0.4f : 0;
+        _rBody.AddRelativeForce(Vector3.Scale(_movementInputVector, MovementMultiplier) * Time.fixedDeltaTime);
+        _rBody.AddRelativeTorque(Vector3.Scale(_rotationInputVector, RotationMultiplier) * Time.fixedDeltaTime);
     }
 
     void OnGUI()
