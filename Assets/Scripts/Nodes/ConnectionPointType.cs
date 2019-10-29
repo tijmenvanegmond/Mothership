8b769a8fd82418ab1681add811f80f42d705f1f8
Tijmen van Egmond <tijmenvanegmond@mdmail.ml>
2019-10-29 18:10:44 +0100

    updated port transforms to work with rotational placements

--- Assets/Scripts/Nodes/ConnectionPointType.cs
+++ Assets/Scripts/Nodes/ConnectionPointType.cs
@@ -4,8 +4,10 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [Serializable]
-public struct ConnectionPointType {
-	public int ID;
-	public string Name;
-	public float RotationStep;
+public struct ConnectionPointType
+{
+    public int ID;
+    public string Name;
+    public float RotationStep;
+    public Vector3 PlacementRotation;
 }
