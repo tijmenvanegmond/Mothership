01dfb0c56f9209f96ff5fdad7bffa2f204e25b4b
TvE <tym_time@hotmail.com>
2020-05-03 18:11:24 +0200

    Ship inherits from construct

--- Assets/Code/Construct.cs
+++ Assets/Code/Construct.cs
@@ -4,69 +4,77 @@ using System.Linq;
 using UnityEngine;
 
 public class Construct : MonoBehaviour {
-    Dictionary<int, Node> nodes;
-    HashSet<Node> myNodes;
+    protected Dictionary<int, Node> nodes;
+    protected HashSet<Node> myNodes;
 
-    public void Start () {
+    public virtual void Start() {
         nodes = NodeController.NodeDict;
-        myNodes = new HashSet<Node> ();
+        myNodes = new HashSet<Node>();
 
-        AddChildNodesToMyNodes ();
+        AddChildNodesToMyNodes();
     }
 
-    private void AddChildNodesToMyNodes () {
+    private void AddChildNodesToMyNodes() {
         foreach (Transform child in transform)
-            AddNode (child.gameObject, false);
+            AddNode(child.gameObject, false);
     }
 
-    public bool AddNode (GameObject newNode, bool checkPlacement = true) {
-        var nodeComponent = newNode.GetComponent<Node> ();
+    public bool AddNode(GameObject newNode, bool checkPlacement = true) {
+        var nodeComponent = newNode.GetComponent<Node>();
         if (nodeComponent == null)
             return false;
-        return AddNode (nodeComponent, checkPlacement);
+        return AddNode(nodeComponent, checkPlacement);
     }
 
-    public bool AddNode (Node newNode, bool checkPlacement = true) {
+    public bool AddNode(Node newNode, bool checkPlacement = true) {
         if (checkPlacement) {
             //Check node placement legit
             var newNodeGO = newNode.gameObject;
             //TODO: check for collisions
-            if (newNodeGO.transform.parent != transform || !newNode.HasViableConnections ()) {
-                Debug.Log ("Node placement failed. \n Node not a child of expected ship or no viable connections.");
-                Destroy (newNodeGO);
+            if (newNodeGO.transform.parent != transform || !newNode.HasViableConnections()) {
+                Debug.Log("Node placement failed. \n Node not a child of expected ship or no viable connections.");
+                Destroy(newNodeGO);
                 return false;
             }
         }
-        newNode.ConnectPorts ();
-        myNodes.Add (newNode);
+        newNode.ConnectPorts();
+        myNodes.Add(newNode);
         return true;
     }
 
-    public void RemoveNode (Node node) {
+    public void RemoveNode(Node node) {
         //Check node connections
         //TODO: the ship should remain the group where the playerchar is seated
-        var nodeSetList = node.CheckIfNodeABridge ().OrderByDescending (g => g.Count ()); //biggest first
-        if (nodeSetList.Count () > 1) {
-            //bigest group remains the ship
-            myNodes = nodeSetList.First ();
-            //split up the ship into groups created
-            foreach (var set in nodeSetList.Skip (1)) {
-                var newShipGO = new GameObject ();
-                newShipGO.name = name + "-breakoff";
-                var newShip = newShipGO.AddComponent<Ship> ();
-                foreach (var aNode in set)
-                    aNode.transform.parent = newShipGO.transform;
-                //TODO: add velocity based on angularVelocity
-                //And better rbody way
-                var newRBody = newShipGO.AddComponent<Rigidbody> () as Rigidbody;
-                newRBody.velocity = rBody.velocity;
-            }
+        var nodeSetList = node.CheckIfNodeABridge().OrderByDescending(g => g.Count()); //biggest first
+        if (nodeSetList.Count() > 1) {
+            HandleBridgeRemoval(nodeSetList);
         }
 
-        myNodes.Remove (node);
-        node.Remove ();
-        //TODO: recalculate ship properties
-        UpdateShip ();
+        myNodes.Remove(node);
+        node.Remove();
+        //TODO: recalculate properties
+        this.UpdateConstruct();
+    }
+
+    protected virtual IEnumerable<GameObject> HandleBridgeRemoval(IEnumerable<HashSet<Node>> nodeSetList) {
+        //bigest group remains the ship
+        myNodes = nodeSetList.First();
+        //split up the ship into groups created
+        var breakoffParts = new List<GameObject>();
+        foreach (var set in nodeSetList.Skip(1)) {
+            var newShipGO = new GameObject();
+            newShipGO.name = name + "-breakoff";
+            var newShip = newShipGO.AddComponent<Ship>();
+            foreach (var aNode in set) {
+                aNode.transform.parent = newShipGO.transform;
+            }
+            breakoffParts.Add(newShipGO);
+        }
+        return breakoffParts;
+    }
+
+    protected virtual void UpdateConstruct() {
+
     }
 
 }
--- Assets/Code/Ship/Ship.cs
+++ Assets/Code/Ship/Ship.cs
@@ -3,21 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
-public class Ship : MonoBehaviour { //should implement construct
+public class Ship : Construct { //should implement construct
     //Max speed in m/s
     //TODO: it's only used for UI V-arrow
     public const float MAX_SPEED = 50f;
     public Rigidbody rBody;
-    Dictionary<int, Node> nodes;
-    HashSet<Node> myNodes;
     public Truster[] Trusters = new Truster[0];
 
-    public void Start () {
-        nodes = NodeController.NodeDict;
-        myNodes = new HashSet<Node> ();
-        rBody = GetComponent<Rigidbody> ();
+    public override void Start() {
+        base.Start();
+        rBody = GetComponent<Rigidbody>();
         if (rBody == null) {
-            rBody = gameObject.AddComponent<Rigidbody> () as Rigidbody;
+            rBody = gameObject.AddComponent<Rigidbody>() as Rigidbody;
             rBody.drag = 0;
             rBody.angularDrag = .3f;
             rBody.useGravity = false;
@@ -25,20 +22,33 @@ public class Ship : MonoBehaviour { //should implement construct
             rBody.collisionDetectionMode = CollisionDetectionMode.Continuous;
         }
 
-        AddChildNodesToMyNodes ();
+        AddChildNodesToMyNodes();
     }
 
-    private void AddChildNodesToMyNodes () {
+    protected override IEnumerable<GameObject> HandleBridgeRemoval(IEnumerable<HashSet<Node>> nodeSetList) {
+        var breakoffParts = base.HandleBridgeRemoval(nodeSetList);
+        foreach (var partGO in breakoffParts) {
+            var newRBody = partGO.AddComponent<Rigidbody>() as Rigidbody;
+            newRBody.velocity = rBody.velocity;
+        }
+        return breakoffParts;
+    }
+
+    protected override void UpdateConstruct() {
+        UpdateShip();
+    }
+
+    private void AddChildNodesToMyNodes() {
         foreach (Transform child in transform)
-            AddNode (child.gameObject, false);
+            AddNode(child.gameObject, false);
     }
 
-    public void UpdateShip () {
-        CalculateMass ();
-        UpdateTrusters ();
+    private void UpdateShip() {
+        CalculateMass();
+        UpdateTrusters();
     }
 
-    float CalculateMass () {
+    private float CalculateMass() {
         float totalMass = 0;
         Vector3 centerOfMass = Vector3.zero;
         foreach (var node in myNodes) {
@@ -50,66 +60,15 @@ public class Ship : MonoBehaviour { //should implement construct
         return totalMass;
     }
 
-    void UpdateTrusters () {
-        Trusters = myNodes.OfType<Truster> ().ToArray ();
-    }
-
-    public bool AddNode (GameObject newNode, bool checkPlacement = true) {
-        var nodeComponent = newNode.GetComponent<Node> ();
-        if (nodeComponent == null)
-            return false;
-        return AddNode (nodeComponent, checkPlacement);
-    }
-
-    public bool AddNode (Node newNode, bool checkPlacement = true) {
-        if (checkPlacement) {
-            //Check node placement legit
-            var newNodeGO = newNode.gameObject;
-            //TODO: check for collisions
-            if (newNodeGO.transform.parent != transform || !newNode.HasViableConnections ()) {
-                Debug.Log ("Node placement failed. \n Node not a child of expected ship or no viable connections.");
-                Destroy (newNodeGO);
-                return false;
-            }
-        }
-        newNode.ConnectPorts ();
-        myNodes.Add (newNode);
-        UpdateShip ();
-        return true;
-    }
-
-    public void RemoveNode (Node node) {
-        //Check node connections
-        //TODO: the ship should remain the group where the playerchar is seated
-        var nodeSetList = node.CheckIfNodeABridge ().OrderByDescending (g => g.Count ()); //biggest first
-        if (nodeSetList.Count () > 1) {
-            //bigest group remains the ship
-            myNodes = nodeSetList.First ();
-            //split up the ship into groups created
-            foreach (var set in nodeSetList.Skip (1)) {
-                var newShipGO = new GameObject ();
-                newShipGO.name = name + "-breakoff";
-                var newShip = newShipGO.AddComponent<Ship> ();
-                foreach (var aNode in set)
-                    aNode.transform.parent = newShipGO.transform;
-                //TODO: add velocity based on angularVelocity
-                //And better rbody way
-                var newRBody = newShipGO.AddComponent<Rigidbody> () as Rigidbody;
-                newRBody.velocity = rBody.velocity;
-            }
-        }
-
-        myNodes.Remove (node);
-        node.Remove ();
-        //TODO: recalculate ship properties
-        UpdateShip ();
+    private void UpdateTrusters() {
+        Trusters = myNodes.OfType<Truster>().ToArray();
     }
 
     ///DEBUG
-    void OnDrawGizmosSelected () {
+    private void OnDrawGizmosSelected() {
         Gizmos.color = Color.yellow;
         if (rBody != null)
-            Gizmos.DrawSphere (transform.TransformPoint (rBody.centerOfMass), rBody.mass * .05f);
+            Gizmos.DrawSphere(transform.TransformPoint(rBody.centerOfMass), rBody.mass * .05f);
     }
 
 }
